5eae89f5b13c9feb251f53f75d7d29d9f9aa9525
ZeroHX <41178369+ZeroHX@users.noreply.github.com>
2018-11-27 22:17:04 +0700

    Update pro1.py

--- file/pro1.py
+++ file/pro1.py
@@ -2,11 +2,16 @@
 Project PSIT61
 Data Analysis
 -ProjectAVG-
+>>Barchart: income/year(each region)
+
+by Jakkawan Intaratchaiyakij
+    61070023
 """
 import pandas as pd
 import pygal
 def main():
-    df = pd.read_csv('region.csv', index_col='Region')
+    df = pd.read_csv('ProjectAVG/income.csv', index_col='Region and province')
+    year = list(df)[1:]
     tdf = df.T
     north = list(tdf['Northern Region'])[1:]
     central = list(tdf['Central Region'])[1:]
@@ -15,10 +20,11 @@ def main():
 
     line_chart = pygal.Bar()
     line_chart.title = 'Income AVG(Region)'
-    line_chart.x_labels = ('2541', '2543', '2545', '2547', '2549', '2550', '2552', '2554', '2556', '2558')
+    line_chart.x_labels = year
     line_chart.add('Central', central)
     line_chart.add('North', north)
     line_chart.add('NorthEast', neast)
     line_chart.add('South', south)
+    line_chart.render_to_file('year_region_chart.svg')
     line_chart.render_in_browser()
 main()

9d950bd2fd442baf0755408b2cc341a5595acef5
ZeroHX <41178369+ZeroHX@users.noreply.github.com>
2018-11-28 11:42:58 +0700

    Up pro3

---
+++ file/pro3.py
@@ -0,0 +1,76 @@
+"""
+Project PSIT61
+Data Analysis
+-ProjectAVG-
+>>Barchart_Horizon: income/another province in region
+
+by  Jakkawan Intaratchaiyakij
+    61070023
+"""
+import pandas as pd
+import pygal
+from pygal.style import DarkStyle
+def main():
+    df = pd.read_csv('income.csv', index_col='Region and province')
+    tdf = df.T
+    central(df, tdf)
+    north(df, tdf)
+    neast(df, tdf)
+    south(df, tdf)
+
+def central(df, tdf):
+    """ Data for Central Region """
+    region = 'Central'
+    cen = df['Central Region':'Northern Region'][1:-2]
+    cen_lst = list(cen.T)
+    lst = []
+    for i in cen_lst:
+        avg = sum(list(cen.T[i])[1:])/len(list(cen.T[i])[1:])
+        lst.append(avg)
+    graph(cen_lst, lst, region)
+
+
+def north(df, tdf):
+    """ Data for Central Region """
+    region = 'Northern'
+    north = df['Northern Region':'Northeastern Region'][1:-2]
+    north_lst = list(north.T)
+    lst = []
+    for i in north_lst:
+        avg = sum(list(north.T[i])[1:])/len(list(north.T[i])[1:])
+        lst.append(avg)
+    graph(north_lst, lst, region)
+
+def neast(df, tdf):
+    """ Data for Central Region """
+    region = 'Northeastern'
+    neast = df['Northeastern Region':'Southern Region'][1:-2]
+    neast_lst = list(neast.T)
+    lst = []
+    for i in neast_lst:
+        avg = sum(list(neast.T[i])[1:])/len(list(neast.T[i])[1:])
+        lst.append(avg)
+    graph(neast_lst, lst, region)
+
+def south(df, tdf):
+    """ Data for Central Region """
+    region = 'Southern'
+    south = df['Southern Region':][1:-2]
+    south_lst = list(south.T)
+    lst = []
+    for i in south_lst:
+        avg = sum(list(south.T[i])[1:])/len(list(south.T[i])[1:])
+        lst.append(avg)
+    graph(south_lst, lst, region)
+
+def graph(region, data, reg):
+    """ Function plot graph """
+    line_chart = pygal.HorizontalBar(fill=True, interpolate='cubic', style=DarkStyle)
+    line_chart.title = '%s Region Income average in each year' % reg
+    for i in range(len(region)):
+        line_chart.add(region[i], data[i])
+    line_chart.render_in_browser()
+    line_chart.render_to_file('%s.svg' % reg)
+
+
+main()
